1a61a030e846a136becb898a4dab76f30b275e3c
YWX <930924708@qq.com>
2025-05-06 17:53:00 +0800

    feat(vite配置): 设置服务器端口和严格端口模式
    - 将服务器端口设置为3000，确保开发环境一致性。
    - 启用严格端口模式，避免端口被占用时自动切换，提升开发效率。

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,8 @@ export default defineConfig({
     }
   },
   server: {
+    port: 3000, // 设置服务器端口为3000
+    strictPort: true, // 如果端口被占用，则会直接退出而不是尝试下一个可用端口
     proxy: {
       '/api': {
         target: 'http://localhost:8081',
